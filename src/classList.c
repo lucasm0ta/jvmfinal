f094e14fc5f8173b6d9d58b8f160dc7d26b4ef68
Renan Lobato Rheinboldt <renan.lobato.rheinboldt@gmail.com>
2017-07-02 17:22:42 -0300

    adicionar funcoes novas classList

--- src/classList.c
+++ src/classList.c
@@ -1,6 +1,12 @@
+#include <stdbool.h>
+#include <wchar.h>
 #include "classList.h"
 
 static lista_classe* g_classList;
+
+char *get_nome_classe(const lista_classe *classListAux, int stringIndex);
+
+bool existe_proximo_elemento(const lista_classe *aux_lista_classe);
 
 lista_classe* criar_lista_classe(){
 	lista_classe* aux = (lista_classe*)malloc(sizeof(lista_classe));
@@ -15,41 +21,33 @@ void iniciar_lista_classe(){
 
 ClassFile* buscar_classe(char *name){
     iniciar_lista_classe();
-	lista_classe* classListAux = g_classList;
+	lista_classe* aux_lista_classe = g_classList;
 	int stringIndex;
 	char *className;
-	while(classListAux->next != NULL){
-		stringIndex = classListAux->classFile->constant_pool[classListAux->classFile->this_class].info.string_info->string_index;
-		className = classListAux->classFile->constant_pool[stringIndex].info.utf8_info->bytes;
-		if(strcmp(className, name) == 0){
-			return classListAux->classFile;
+	while(existe_proximo_elemento(aux_lista_classe)){
+		if(strcmp(get_nome_classe(aux_lista_classe, stringIndex), name) == 0){
+			return aux_lista_classe->classFile;
 		}
-		classListAux = classListAux->next;
+		aux_lista_classe = aux_lista_classe->next;
 	}
 	return NULL;
+}
+
+bool existe_proximo_elemento(const lista_classe *aux_lista_classe) { return aux_lista_classe->next != NULL; }
+
+char *get_nome_classe(const lista_classe *classListAux, int stringIndex) {
+	char *className;
+	stringIndex = classListAux->classFile->constant_pool[classListAux->classFile->this_class].info.string_info->string_index;
+	className = classListAux->classFile->constant_pool[stringIndex].info.utf8_info->bytes;
+	return className;
 }
 
 void empilhar_classe(ClassFile *classFile){
     iniciar_lista_classe();
 	lista_classe* aux_lista_classe = g_classList;
-	while(aux_lista_classe->next != NULL){
+	while(existe_proximo_elemento(aux_lista_classe)){
 		aux_lista_classe = aux_lista_classe->next;
 	}
 	aux_lista_classe->classFile = classFile;
 	aux_lista_classe->next = criar_lista_classe();
-}
-
-
-void dumpClassList(){
-    iniciar_lista_classe();
-	lista_classe* classListAux = g_classList;
-	int stringIndex;
-	char *className;
-	printf("Dump Class List : \n");
-	for (int i = 0; classListAux->next != NULL; ++i){
-		stringIndex = classListAux->classFile->constant_pool[classListAux->classFile->this_class].info.string_info->string_index;
-		className = classListAux->classFile->constant_pool[stringIndex].info.utf8_info->bytes;
-		printf("%d : %s\n", i, className);
-		classListAux = classListAux->next;
-	}
 }
